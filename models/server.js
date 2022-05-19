760e8daa503115850a5aa63c5cd01015b62a1ffc
Marcelo Acevedo <93295344+miqueas35@users.noreply.github.com>
2022-05-19 16:08:17 -0300

    agregando jwt

--- models/server.js
+++ models/server.js
@@ -6,6 +6,7 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.authPath = "/api/auth";
     this.usersPath = "/api/users";
 
     //conectar BD
@@ -36,6 +37,7 @@ class Server {
 
   // rutas
   routes() {
+    this.app.use(this.authPath, require("../routes/auth"));
     this.app.use(this.usersPath, require("../routes/users"));
   }
 

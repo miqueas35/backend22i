4217a747fa8c9cf201f5c260e73b897d9c86e9d0
Marcelo Acevedo <93295344+miqueas35@users.noreply.github.com>
2022-05-23 10:13:47 -0300

    finalizando controlador de categorias

--- controllers/categoria.js
+++ controllers/categoria.js
@@ -1,37 +1,93 @@
+const { response } = require("express");
 const { Categoria } = require("../models/categoria");
+const usuario = require("../models/usuario");
 
 //obtener todas las categorias paginadas y traeremos los datos del usuario que creo la categoria
-const obtenerCategorias = (req, res) => {
+const obtenerCategorias = async (req, res) => {
+  const { limite = 5, desde = 0 } = req.query;
+
+  const query = { estado: true };
+
+  const [total, categorias] = await Promise.all([
+    Categoria.countDocuments(query),
+    Categoria.find(query)
+      .skip(Number(desde))
+      .limit(Number(limite))
+      .populate("usuario", "nombre correo"),
+  ]);
+
   res.json({
-    msg: "GET Categoria",
+    total,
+    categorias,
   });
 };
 
 //obtener una categoria por su id y los datos del usuario
-const obtenerCategoria = (req, res) => {
+const obtenerCategoria = async (req, res = response) => {
+  const { id } = req.params;
+
+  const categoria = await Categoria.findById(id).populate(
+    "usuario",
+    "nombre correo"
+  );
+
   res.json({
-    msg: "GET Categoria por id",
+    categoria,
   });
 };
 
 //crear una categoria
-const crearCategoria = (req, res) => {
-  res.json({
-    msg: "POST Categoria",
-  });
+const crearCategoria = async (req, res = response) => {
+  //validar si no hay otra categoria igual
+  const nombre = req.body.nombre.toUpperCase();
+  const categoriaDB = await Categoria.findOne({ nombre });
+
+  if (categoriaDB) {
+    res.status(400).json({
+      msg: `La categoria ${categoriaDB.nombre} ya existe`,
+    });
+  }
+
+  //generar la data para guardar en la BD
+  const data = {
+    nombre,
+    usuario: req.usuario._id,
+  };
+
+  const categoria = new Categoria(data);
+
+  //grabar en la BD
+  await categoria.save();
+  res.status(201).json(categoria);
 };
 
 //actualizar la categoria, validar el nombre
-const actualizarCategoria = (req, res) => {
-  res.json({
-    msg: "PUT Categoria",
-  });
+const actualizarCategoria = async (req, res) => {
+  const { id } = req.params;
+  const nombre = req.body.nombre.toUpperCase();
+  const usuario = req.usuario._id;
+
+  const data = {
+    nombre,
+    usuario,
+  };
+
+  const categoria = await Categoria.findByIdAndUpdate(id, data, { new: true });
+  res.status(201).json(categoria);
 };
 
 //borrar la categoria
-const borrarCategoria = (req, res) => {
+const borrarCategoria = async (req, res) => {
+  const { id } = req.params;
+
+  const categoriaBorrada = await Categoria.findByIdAndUpdate(
+    id,
+    { estado: false },
+    { new: true }
+  );
+
   res.json({
-    msg: "DELETE Categoria",
+    categoriaBorrada,
   });
 };
 
